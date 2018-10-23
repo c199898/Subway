2a045a2cfdb6c661b61ec620775875685e0902d8
cxy <c199898@126.com>
2018-10-24 00:12:02 +0800

    try

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,7 @@
 public class Test {
 	
     public static void main(String[] args) {
-             Test t=new Test();
-             Function f=new Function();
-             f.search();
+
 }  
     
 }
