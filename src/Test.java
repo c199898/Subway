f896a7f29362a81cc77da8a46a13ee23dbf141fe
cxy <c199898@126.com>
2018-10-25 23:50:12 +0800

    尝试实现功能3

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,11 @@
 public class Test {
 	
     public static void main(String[] args) {
-       Function f=new Function();
-       String[] ss;
-       ss=f.toEnd("7号线","野芷湖");
-       for(int i=0;i<ss.length;i++) {
-    	   System.out.println(ss[i]);
+    	Read r=new Read();
+           SearchPath s=new SearchPath();
+          System.out.println(s.calculateDistance(0,4)); 
        }
         
 }  
     
-}
+
